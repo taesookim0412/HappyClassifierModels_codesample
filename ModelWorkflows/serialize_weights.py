79e1957e3cdd61a888b76644cee06df6fc57681d
taesookim0412 <taesoo.kim0412@gmail.com>
2022-06-18 21:40:19 -0700

    preparing util code for inference cluster

---
+++ ModelWorkflows/serialize_weights.py
@@ -0,0 +1,60 @@
+import os
+import joblib
+import tensorflow as tf
+import Models.joy_and_anger.joy_and_anger_utils as joy_and_anger_lib_utils
+
+# export our trained vocab list since this wasn't done in the training process
+def export_vocab_list(data_fp):
+    """
+    Creates a vocab txt file from train.txt and saves it in the same folder as joy_and_anger_vocab.txt
+    :param data_fp: full path for train.txt
+    :return:
+    """
+    res = []
+    with open(data_fp, "r+") as f:
+        lines = f.readlines()
+        for line in lines:
+            # if the line has multiple semi-colons it will break.
+            vocab_line, label = line.split(";")
+            label = label.strip()
+            if label == "joy" or label == "anger":
+                res.append(vocab_line)
+    target_fp = os.path.join(os.path.dirname(data_fp), "joy_and_anger_vocab.txt")
+    with open(target_fp, "w+") as f:
+        f.write("\n".join(res))
+
+def load_saved_model_locally(model_fn):
+    """
+    Loads a deployed model into memory
+    :param model_fn: name of the model
+    :return: model with adapted TextVectorization and loaded weights
+    """
+    model = joy_and_anger_lib_utils.create_model(None, os.path.join(os.getcwd(), "..", "datasets", "emotions", "joy_and_anger_vocab.txt"))
+    print(type(model))
+    model.load_weights(os.path.join(os.getcwd(), "..", "Models", model_fn, model_fn))
+
+    return model
+
+
+# pickle is not compatible with the preprocessing of TextVectorization
+# pickle is also discouraged from usage in production code.
+# pickle reference is kept for future implementations if it will become necessary
+
+# def pickle_model(tf_model_name:str, pickle_model_name:str):
+#     '''
+#     Loads a tf SavedModel model and pickles the object.
+#     input:
+#     tf_model_name: tf model name in string in ../training/models/*tf_model_name*
+#     pickle_model_name: output pickled model name in string in ../Models/pickled/*pickle_model_name*
+#     '''
+#     tf_model_path = os.path.join(os.getcwd(), "..", "training", "models", tf_model_name)
+#
+#     model = tf.keras.models.load_model(tf_model_path)
+#     dest = os.path.join(os.getcwd(), "..", "Models", "pickled", pickle_model_name)
+#     with open(dest, 'wb') as f:
+#         joblib.dump(model, f)
+#
+# def test_model(model_fn, input):
+#     with open(os.path.join(os.getcwd(), "..", "Models", "pickled", model_fn), "rb") as f:
+#         model = joblib.load(f)
+#         print(model.predict(input))
---
+++ Models/joy_and_anger/joy_and_anger_utils.py
@@ -0,0 +1,61 @@
+from typing import List
+
+import tensorflow as tf
+import os
+
+
+def create_model(encoder: tf.keras.layers.TextVectorization, vocab_list_fp: str) -> tf.keras.Model:
+    '''
+    V1 06.18.2022,
+    Post-creation
+    :param encoder: Adapted TextVectorization layer or None
+    :param vocab_list_fp: Full path for processed list of vocab
+    :return: model
+    '''
+    VOCAB_SIZE = 1500
+    input = tf.keras.Input((1,), dtype=tf.dtypes.string)
+    if not encoder:
+        encoder = tf.keras.layers.TextVectorization(max_tokens=VOCAB_SIZE)
+        # Adapt a vocab list from the training data
+        with open(vocab_list_fp, "r+") as f:
+            lines = f.readlines()
+            lines = [line.strip() for line in lines]
+            encoder.adapt(lines)
+
+    layer = encoder(input)
+    layer = tf.keras.layers.Embedding(input_dim=VOCAB_SIZE,
+                                      output_dim=64,
+                                      mask_zero=True)(layer)
+    layer = tf.keras.layers.Bidirectional(layer=tf.keras.layers.LSTM(units=64))(layer)
+    layer = tf.keras.layers.Dense(64, activation="relu")(layer)
+    layer = tf.keras.layers.Dense(1)(layer)
+
+    return tf.keras.Model(inputs=input, outputs=layer, name="nlp_rnn_classifier")
+
+def load_data(fn:str, fp=None):
+    '''
+    V1 06.18.2022,
+    Post-creation
+    :param fn: local file name
+    :param fp: full path for dataset
+    :return: map of { text : emotion }
+    '''
+    if fp:
+        txt_path = fp
+    else:
+        txt_path = os.path.join(os.getcwd(), "..", "datasets", "emotions", fn)
+
+    res = {}
+    with open(txt_path) as f:
+        lines = f.readlines()
+        for line in lines:
+            line_split = line.split(";")
+            line, emotion = line_split
+            emotion = emotion.strip()
+            if emotion == 'joy' or emotion == 'anger':
+                res[line] = emotion.strip()
+    print(f'loaded {len(res)} items')
+    return res
+
+def load_data_externally(fp: str):
+    return load_data(None, fp)
